752fdf3320c8adbd710d900ebfc6412bd3c14a37
sobchenyuk <ablehands@ukr.net>
2017-04-02 22:19:23 +0300

    first commit

--- homework_1/two.php
+++ homework_1/two.php
@@ -1,29 +1,64 @@
 <?php
-class Two{
-    private $age;
+
+class Two
+{
+    public $age;
+
     function __construct($age)
     {
         $this->age = $age;
     }
+
     public function performance()
     {
         $age = $this->age;
+
         if($age <= 4)
         {
             echo "Вам пора в детский сад";
 
-        }elseif ($age >= 6 || $age <= 17)
+        }
+        else if ($age >= 6 && $age <= 17)
         {
             echo "Вам пора в школу";
 
-        }elseif ($age >= 18 || $age <= 65)
+        }
+        else if ($age >= 18 && $age <= 65)
         {
             echo "Вам пора на работу";
 
         }
     }
+
 }
-$two = new Two("34");
+
+class Three extends Two
+{
+
+    public function performance()
+    {
+        $age = $this->age;
+
+        switch ($age)
+        {
+            case $age <= 4:
+                echo "Вам пора в детский сад";
+                break;
+            case $age >= 6 && $age <= 17:
+                echo "Вам пора в школу";
+                break;
+            case $age >= 18 && $age <= 65:
+                echo "Вам пора на работу";
+                break;
+        }
+
+    }
+}
 
 
+$two = new Two(34);
 $two->performance();
+echo "<br />";
+$three = new Three(34);
+$three->performance();
+
