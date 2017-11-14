5356a8a9550f41e919a7589ca63edb6fba73cdd3
sobchenyuk <ablehands@ukr.net>
2017-11-14 15:37:49 +0200

    Add files via upload

---
+++ Http/Main.php
@@ -0,0 +1,7 @@
+<?php namespace App\Models;
+
+class Main extends SiteModel {
+    public $table = 'main_settings';
+    public $timestamps = false;
+
+}
---
+++ Http/MainForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 10.11.2017
+ * Time: 18:11
+ */
+
+namespace app\Models;
+
+
+class MainForm extends Main
+{
+
+}
